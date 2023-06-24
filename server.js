259f8ba0480f84f43efb47b83e38db4ca374fef4
nandur9 <123826181+nandur9@users.noreply.github.com>
2023-06-24 14:58:45 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,59 @@
+require("dotenv").config();
+const { application } = require("express");
+const express = require("express");
+const app = express();
+const path = require("path");
+const { logger, logEvents } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3500;
+
+// mongoose.set("strictQuery", false);
+
+// mongoose.connect(process.env.MONGO_URL);
+
+app.use(logger);
+
+connectDB();
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use("/", express.static(path.join(__dirname, "public")));
+
+app.use("/", require("./routes/root"));
+
+app.use("/users", require("./routes/userRoutes"));
+
+app.all("*", (req, res) => {
+	res.status(404);
+	if (req.accepts("html")) {
+		res.sendFile(path.join(__dirname, "views", "404.html"));
+	} else if (req.accepts("join")) {
+		res.json({ message: "404 Not Found" });
+	} else {
+		res.type("txt").send("404 Not Found");
+	}
+});
+
+app.use(errorHandler);
+
+mongoose.connection.once("open", () => {
+	console.log("Connected to MongoDB");
+	app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+});
+
+mongoose.connection.on("error", (err) => {
+	console.log(err);
+	logEvents(
+		`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+		"mongoErrLog.log"
+	);
+});
